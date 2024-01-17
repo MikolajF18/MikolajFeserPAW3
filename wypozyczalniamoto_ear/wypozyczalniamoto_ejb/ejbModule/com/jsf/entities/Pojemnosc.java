45a4b36bf96f2c57e1c1f754974f9ec3e2267d7e
MikolajF18 <135436056+MikolajF18@users.noreply.github.com>
2024-01-18 00:12:57 +0100

    Add files via upload

---
+++ wypozyczalniamoto_ear/wypozyczalniamoto_ejb/ejbModule/com/jsf/entities/Pojemnosc.java
@@ -0,0 +1,68 @@
+package com.jsf.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the pojemnosc database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Pojemnosc.findAll", query="SELECT p FROM Pojemnosc p")
+public class Pojemnosc implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idpojemnosci;
+
+	private int pojemnosc;
+
+	//bi-directional many-to-one association to Motocykl
+	@OneToMany(mappedBy="pojemnosc")
+	private List<Motocykl> motocykls;
+
+	public Pojemnosc() {
+	}
+
+	public int getIdpojemnosci() {
+		return this.idpojemnosci;
+	}
+
+	public void setIdpojemnosci(int idpojemnosci) {
+		this.idpojemnosci = idpojemnosci;
+	}
+
+	public int getPojemnosc() {
+		return this.pojemnosc;
+	}
+
+	public void setPojemnosc(int pojemnosc) {
+		this.pojemnosc = pojemnosc;
+	}
+
+	public List<Motocykl> getMotocykls() {
+		return this.motocykls;
+	}
+
+	public void setMotocykls(List<Motocykl> motocykls) {
+		this.motocykls = motocykls;
+	}
+
+	public Motocykl addMotocykl(Motocykl motocykl) {
+		getMotocykls().add(motocykl);
+		motocykl.setPojemnosc(this);
+
+		return motocykl;
+	}
+
+	public Motocykl removeMotocykl(Motocykl motocykl) {
+		getMotocykls().remove(motocykl);
+		motocykl.setPojemnosc(null);
+
+		return motocykl;
+	}
+
+}
---
+++ wypozyczalniamoto_ear/wypozyczalniamoto_ejb/ejbModule/com/jsf/entities/Typ.java
@@ -0,0 +1,68 @@
+package com.jsf.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the typ database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Typ.findAll", query="SELECT t FROM Typ t")
+public class Typ implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idtypu;
+
+	private String typ;
+
+	//bi-directional many-to-one association to Motocykl
+	@OneToMany(mappedBy="typ")
+	private List<Motocykl> motocykls;
+
+	public Typ() {
+	}
+
+	public int getIdtypu() {
+		return this.idtypu;
+	}
+
+	public void setIdtypu(int idtypu) {
+		this.idtypu = idtypu;
+	}
+
+	public String getTyp() {
+		return this.typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public List<Motocykl> getMotocykls() {
+		return this.motocykls;
+	}
+
+	public void setMotocykls(List<Motocykl> motocykls) {
+		this.motocykls = motocykls;
+	}
+
+	public Motocykl addMotocykl(Motocykl motocykl) {
+		getMotocykls().add(motocykl);
+		motocykl.setTyp(this);
+
+		return motocykl;
+	}
+
+	public Motocykl removeMotocykl(Motocykl motocykl) {
+		getMotocykls().remove(motocykl);
+		motocykl.setTyp(null);
+
+		return motocykl;
+	}
+
+}
